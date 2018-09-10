381df74e87c2677eb8da8e97314519e47b375365
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-09-10 16:33:22 -0400

    Update revision

--- Formula/rpg-card-generator.rb
+++ Formula/rpg-card-generator.rb
@@ -4,7 +4,7 @@ class RpgCardGenerator < Formula
   url "https://github.com/Samasaur1/RPG-card-generator/releases/download/v0.4.0/cardgen-0.4.0.tar.gz"
   sha256 "47a64b60e012fdb377424fc7038bb234f21a8a273548b92c291dd3ea23744199"
   version "0.4.0"
-  revision 8
+  revision 9
 
   bottle :unneeded
   
