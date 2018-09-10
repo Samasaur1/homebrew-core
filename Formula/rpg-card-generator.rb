7f0bc547fcc3f8458ed6e6f2c68769baf0047334
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-09-10 09:26:15 -0400

    Update rpg-card-generator.rb

--- Formula/rpg-card-generator.rb
+++ Formula/rpg-card-generator.rb
@@ -4,11 +4,17 @@ class RpgCardGenerator < Formula
   url "https://github.com/Samasaur1/RPG-card-generator/releases/download/v0.4.0/cardgen-0.4.0.tar.gz"
   sha256 "47a64b60e012fdb377424fc7038bb234f21a8a273548b92c291dd3ea23744199"
   version "0.4.0"
+  revision 8
 
   bottle :unneeded
   
   def install
     bin.install "cardgen"
-    system "/usr/local/bin/cardgen RPGSTDLIB"
+  end
+  
+  def caveats
+    <<~EOS
+      Run 'cardgen RPGSTDLIB' to load the standard library
+    EOS
   end
 end
