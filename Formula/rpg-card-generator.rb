6d552c3eddb13b87eb70b750ac1992e53395676d
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-09-10 16:53:32 -0400

    RPG-card-generator v0.4.1

--- Formula/rpg-card-generator.rb
+++ Formula/rpg-card-generator.rb
@@ -1,21 +1,14 @@
 class RpgCardGenerator < Formula
   desc "A card generator to interface with https://crobi.github.io/rpg-cards/generator/generate.html"
   homepage "https://github.com/Samasaur1/RPG-card-generator"
-  url "https://github.com/Samasaur1/RPG-card-generator/releases/download/v0.4.0/cardgen-0.4.0.tar.gz"
-  sha256 "47a64b60e012fdb377424fc7038bb234f21a8a273548b92c291dd3ea23744199"
-  version "0.4.0"
-  revision 9
+  url "https://github.com/Samasaur1/RPG-card-generator/releases/download/v0.4.1/cardgen-0.4.1.tar.gz"
+  sha256 "a68e7ba04268f6e90844f49937a1d1c3f1095ca1ebe7da1899f5c617452eb856"
+  version "0.4.1"
 
   bottle :unneeded
   
   def install
     bin.install "cardgen"
     system "#{bin}/cardgen", "RPGSTDLIB"
-  end
-  
-  def caveats
-    <<~EOS
-      Run 'cardgen RPGSTDLIB' to load the standard library
-    EOS
   end
 end
