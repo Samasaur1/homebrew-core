3b9a60cf4d017c0837a8e1a8e0bf3eaf4211e7e6
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-10-08 08:26:03 -0400

    Attempt to fix installing RPGSTDLIB

--- Formula/rpg-card-generator.rb
+++ Formula/rpg-card-generator.rb
@@ -4,7 +4,7 @@ class RpgCardGenerator < Formula
   url "https://github.com/Samasaur1/RPG-card-generator/archive/v0.4.1.tar.gz"
   sha256 "0c9606a3e6a2a5a17bc97e8fb0e9332cebfaa01fa2182d2a5186f0a04a59639b"
   version "0.4.1"
-  revision 5
+  revision 6
 
   depends_on :xcode
 
@@ -13,12 +13,16 @@ class RpgCardGenerator < Formula
     # see more details here https://github.com/Homebrew/brew/pull/4147
     ENV["CC"] = Utils.popen_read("xcrun -find clang").chomp
 
+    ohai "Installing RPGSTDLIB"
+    system("swift run RPGCardGenerator RPGSTDLIB")
+    ohai "RPGSTDLIB successfully installed"
+    system("swift package clean")
     build_path = "#{buildpath}/.build/release/cardgen"
     ohai "Building Cardgen"
     system("swift build --disable-sandbox -c release -Xswiftc -static-stdlib")
     mv "#{buildpath}/.build/release/RPGCardGenerator", "#{buildpath}/.build/release/cardgen"
     bin.install build_path
-    system("swift run RPGCardGenerator RPGSTDLIB")
+    ohai "Successfully installed Cardgen"
   end
 
   def caveats
