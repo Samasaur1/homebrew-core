4c885c1cc69f2db659b63134e8298f8679f3e486
Sam <30577766+Samasaur1@users.noreply.github.com>
2018-10-08 08:34:41 -0400

    Stop trying to install RPGSTDLIB

--- Formula/rpg-card-generator.rb
+++ Formula/rpg-card-generator.rb
@@ -4,7 +4,7 @@ class RpgCardGenerator < Formula
   url "https://github.com/Samasaur1/RPG-card-generator/archive/v0.4.1.tar.gz"
   sha256 "0c9606a3e6a2a5a17bc97e8fb0e9332cebfaa01fa2182d2a5186f0a04a59639b"
   version "0.4.1"
-  revision 8
+  revision 9
 
   depends_on :xcode
 
@@ -17,7 +17,6 @@ class RpgCardGenerator < Formula
     ohai "Building Cardgen"
     system("swift build --disable-sandbox -c release -Xswiftc -static-stdlib")
     mv "#{buildpath}/.build/release/RPGCardGenerator", "#{buildpath}/.build/release/cardgen"
-    system("#{build_path} RPGSTDLIB")
     bin.install build_path
     ohai "Successfully installed Cardgen"
   end
