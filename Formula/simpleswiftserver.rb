03be1b83fe93fb1a1a966066f8222bd74a2795e1
Sam <samgauck@icloud.com>
2018-11-11 11:28:07 -0500

    Update SimpleSwiftServer to 1.2.1

--- Formula/simpleswiftserver.rb
+++ Formula/simpleswiftserver.rb
@@ -1,13 +1,10 @@
 class Simpleswiftserver < Formula
   desc "A Swift executable equivalent to Python's SimpleHTTPServer"
   homepage "https://github.com/Samasaur1/SimpleSwiftServer"
-  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v1.2.0.tar.gz"
-  sha256 "86e572f0774132f2eca2b8bd04ce4c619654ad75590460c436c98f75212f7ca6"
-  version "1.2.0"
+  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v1.2.1.tar.gz"
+  sha256 "9b3e127f34bcdf64afd4e3ccc0f290fcd4c57e1fda10a9c5973774698d41a360"
+  version "1.2.1"
   head "https://github.com/Samasaur1/SimpleSwiftServer.git"
-  revision 1
-
-  depends_on :xcode
 
   def install
     # fixes an issue an issue in homebrew when both Xcode 9.3+ and command line tools are installed
