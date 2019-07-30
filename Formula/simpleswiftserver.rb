a048caa7adf0b6469a7de1bc074adee9147bdecc
Sam <samgauck@icloud.com>
2019-07-30 13:20:25 -0400

    Update SimpleSwiftServer to 2.0.0

--- Formula/simpleswiftserver.rb
+++ Formula/simpleswiftserver.rb
@@ -1,8 +1,8 @@
 class Simpleswiftserver < Formula
   desc "A Swift executable equivalent to Python's SimpleHTTPServer"
   homepage "https://github.com/Samasaur1/SimpleSwiftServer"
-  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v1.2.1.tar.gz"
-  sha256 "9b3e127f34bcdf64afd4e3ccc0f290fcd4c57e1fda10a9c5973774698d41a360"
+  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v2.0.0.tar.gz"
+  sha256 "b1c75b9a12b1ed8f7ad58bf6e9fabf8ab20d724543746b77bdc6ad9a5f0db909"
   version "1.2.1"
   head "https://github.com/Samasaur1/SimpleSwiftServer.git"
 
