e95eb6f8a7a39eca5c0a6b75889a25e7059fb12c
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2020-12-14 11:44:28 -0500

    Update SimpleSwiftServer to v4.3.0 (installs man page)

--- Formula/simpleswiftserver.rb
+++ Formula/simpleswiftserver.rb
@@ -1,11 +1,10 @@
 class Simpleswiftserver < Formula
   desc "A Swift executable equivalent to Python's SimpleHTTPServer — plus more!"
   homepage "https://samasaur1.github.io/projects/simpleswiftserver.html"
-  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v4.2.0.tar.gz"
-  sha256 "6fe3f61d26a2f20388b41b15f865cc0aa955b02ae939e93c4d3da91ce76823a5"
-  version "4.2.0"
+  url "https://github.com/Samasaur1/SimpleSwiftServer/archive/v4.3.0.tar.gz"
+  sha256 "1602ebcebac5292de5fd5f5b414b45ee00aad165819d6b50153370955a8099e8"
+  version "4.3.0"
   head "https://github.com/Samasaur1/SimpleSwiftServer.git"
-  revision 2
 
   depends_on :xcode
 
@@ -18,5 +17,6 @@ class Simpleswiftserver < Formula
     ohai "Building SimpleSwiftServer"
     system("swift build --disable-sandbox -c release")
     bin.install build_path
+    man1.install "server.1"
   end
 end
